6acece6ded75587d711f86912e61ed7e52eb5ba1
Karan Talapalli <karan.t@volgainfotech.com>
2023-03-31 17:20:04 +0530

    change baseURL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://inventory-management-backend.onrender.com",
-  baseURL: "http://localhost:3200",
+  baseURL: "https://inventory-management-backend.onrender.com",
+  // baseURL: "http://localhost:3200",
 });
 
 export default api;
